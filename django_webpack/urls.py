3ecade2c8c38b05ab14a154f58c76aaabd08940c
Josef Rousek <josef.rousek@gmail.com>
2019-04-16 14:27:59 +0200

    Create Django app

--- django_webpack/urls.py
+++ django_webpack/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from things import views
 
 urlpatterns = [
+    path("", views.HomeView.as_view()),
     path("admin/", admin.site.urls)
 ]
